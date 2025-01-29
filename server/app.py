4eb8fe7b77cb9baa36c0f35e583696df76227fd1
George Yegon <georgeyegon67@gmail.com>
2025-01-29 08:44:20 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,23 +1,14 @@
-#!/usr/bin/env python3
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import db
+app = Flask(__name__)
 
-# Standard library imports
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['SQLACHEMY_TRACK_MODIFICATION'] = False
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+migrate = Migrate(app, db)
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+db.init_app(app)
 
-
-# Views go here!
-
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
-
-
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
-
+api = Api(app)
