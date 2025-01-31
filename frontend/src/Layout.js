fba5d283b8ccdcdce60b7a1f5e0673318bc6ecd3
georgeyegon <georgeyegon67@gmail.com>
2025-01-31 18:41:08 +0300

    Update Layout.js

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -20,7 +20,7 @@ const Layout = () => {
   const handleLogin = async (username, password) => {
     try {
       const loadingToast = toast.loading('Logging in...');
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('https://events-manager-5wr8.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
