75c2a201d8e26e3e4c012500a43e46494d0e6475
georgeyegon <georgeyegon67@gmail.com>
2025-01-31 18:37:33 +0300

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -16,7 +16,7 @@ function Login() {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('https://events-manager-5wr8.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
